73ff172f3c51c45f403568667c1f56151b11bc74
nzaih1999 <nzaih18@gmail.com>
2021-12-28 14:37:54 +0300

    first commit

---
+++ src/components/header/header.data.js
@@ -0,0 +1,21 @@
+export default [
+  {
+    path: 'home',
+    label: 'Home',
+  },
+  {
+    path: 'services',
+    label: 'Services',
+  },
+  {
+    path: 'about',
+    label: 'About',
+  },
+  {
+    path: 'blog',
+    label: 'Updates',
+  },
+
+  
+
+];
---
+++ src/pages/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { ThemeProvider } from 'theme-ui';
+import theme from 'theme';
+import SEO from 'components/seo';
+import Layout from 'components/layout';
+import Banner from 'sections/banner';
+import Subscribe from 'sections/subscribe';
+import Services from 'sections/services';
+import VideoIntro from 'sections/video-intro';
+import WorldMap from 'sections/world-map';
+import Milestone from 'sections/milestone';
+import Blog from 'sections/blog';
+
+export default function IndexPage() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <SEO
+          title="Live For Children"
+          description="Help a child and save a life"
+        />
+        <Banner />
+        <Services />
+        <VideoIntro />
+        <WorldMap />
+        <Milestone />
+        <Blog />
+        <Subscribe />
+      </Layout>
+    </ThemeProvider>
+  );
+}
